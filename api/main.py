941b32162f66342af98c87aec6b7617753a769d3
eddyizm <eddyizm@gmail.com>
2022-02-13 19:18:51 -0800

    changing root backend api url

--- api/main.py
+++ api/main.py
@@ -4,7 +4,7 @@ from routers import sightings
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
     
-app = FastAPI()
+app = FastAPI(root_path="/api/v1")
 
 origins = [
     "http://localhost",
